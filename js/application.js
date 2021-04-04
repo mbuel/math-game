25cb382f08e8491ec28c4387daa266a17292e755
mbuel <morris.buel@gmail.com>
2021-04-04 10:02:10 -0700

    rough class setup, timer created and functional

--- js/application.js
+++ js/application.js
@@ -1,112 +1,49 @@
-'use strict';
+// TODO: create class
+class MathLogic {
+    highScore = 0;
+    playerScore = 0;
+    timerMax = 10;
+    currentTimer = 0;
 
-// template string 
-var templateString = `<!-- Template Row to add per item -->
-      <div class="row grocery-item text-center">
-        <div class="item-name col-3">
-          <!-- item name -->
-          %ITEM%
-        </div>
-        <div class="cost col-3">
-          <!-- item price -->
-          <input type="number" value="%PRICE%" />
-        </div>
-        <div class="qty col-3">
-          <!-- item quantity -->
-          <input type="number" value="%QTY%" />
-        </div>
-        <div class="col-3">
-          <!-- item remove -->
-          <div class="summary row">
-            <div class="total col-6 text-success">
-              %TOTAL%
-            </div>
-            <div class="col-6">
-              <div class="btn remove btn-danger text-center rounded-pill"><i class="fa fa-remove"></i></div>
-            </div>
-          </div>
-        </div>
-      </div>`;
-
-// Currency Formatter
-// https://flaviocopes.com/how-to-format-number-as-currency-javascript/
-const formatter = new Intl.NumberFormat('en-US', {
-    style: 'currency',
-    currency: 'USD',
-    minimumFractionDigits: 2
-  })
-  
-
-var updateRowTotalCost = function(row) {
-    var itemQty = parseFloat($(row).find('.qty input').val());
-    var itemCost = parseFloat($(row).find('.cost input').val());
-
-    var rowCostTotal = itemQty * itemCost;
-    $(row).find('.total').text(formatter.format(rowCostTotal));
-    
-    return {
-        rowCostTotal: rowCostTotal,
-        itemQty : itemQty
-    };
-}
-
-var updateTotalCosts = function() {
-    var totalCost = 0;
-    var totalItems = 0;
-    $('.list > .grocery-item').each(function(i, ele) {
-        var {rowCostTotal, itemQty} = updateRowTotalCost(ele);
-        console.log(rowCostTotal, itemQty);
-        totalItems += itemQty;
-        totalCost += rowCostTotal;
-    });
-    $('#total-items').text(Math.floor(totalItems));
-    $('#total-cost').text(formatter.format(totalCost));
-}
-
-var addItemToList = function() {
-    var item = $('#new-item').val();
-    var qty = $('#new-qty').val();
-    var cost = $('#new-cost').val();
-    
-    console.log(item, qty, cost);
-
-    // check values
-
-    if (!item || !qty || !cost) {
-        alert('Cannot add this item to the list.');
-        return;
+    constructor() {
+        $('#player-input').on('click', function() {
+            // TODO: start timer
+            console.log('timer start');
+            this.startTimer();
+        }.bind(this));
+        $('#btn-answer').on('click', this.verifyAnswer);
+        $(document).on('keypress', function(e) {
+            if(e.originalEvent.key === 'Enter') {
+                this.verifyAnswer();
+            }
+        }.bind(this));
     }
 
-    var item = templateString.replace(/%ITEM%/, item).replace(/%QTY%/, qty).replace(/%PRICE%/, cost);
+    startTimer() {
+        this.timer = setInterval(() => {
+            this.currentTimer++;
+            console.log(this.currentTimer, this.timerMax);
+            var currentTime = this.timerMax - this.currentTimer;
+            if (currentTime >= 0) {
+                $('#countdown').text(currentTime);
 
-    $('.list').append(item);
+            } else {
+                clearInterval(this.timer);
+            }
+        }, 1000);
+    }
 
-    updateTotalCosts();
-}
-
-
-var timeout;
-var updateList = function() {
-    clearTimeout(timeout);
-    timeout = setTimeout(function () {
-        updateTotalCosts();
-    }, 250);
+    verifyAnswer() {
+        console.log('Verifying answer!');
+    }
 }
 
 $(document).ready(function () {
-    updateTotalCosts();
-    
-    $('.list').parent().on('click', '.remove', function (event) {
-
-        $(this).parent().parent().parent().parent().remove();
-        updateTotalCosts();
-
-    });
-    
-    $('.list').on('input', updateList);
-
-    $('.add').on('click', addItemToList); 
-
-
-
+    console.log('loading')
+    new MathLogic();
 });
+// TODO: iniate first random question
+
+// TODO: track current score and high score
+
+// TODO: determine operand
